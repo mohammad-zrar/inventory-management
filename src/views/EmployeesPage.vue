325bcd02b207ea52fdfb9cc562863ad4150872a9
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-03-12 12:55:47 +0300

    feat(ProductPage.vue): Add demo table into ProductPage.vue

--- src/views/EmployeesPage.vue
+++ src/views/EmployeesPage.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Employees</h1>
+  <div class="tw-container tw-mx-auto tw-p-4"></div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur minima
+    dolorum quae, dolore fuga inventore minus architecto iste accusamus, porro
+    possimus optio laudantium necessitatibus dicta tempora. Recusandae odio
+    minima placeat tempore unde!
+  </p>
+  <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel debitis ipsam
+    quo sed, quos voluptatibus adipisci distinctio repellat, numquam, incidunt
+    minima laudantium aperiam ratione tempora soluta obcaecati maiores expedita
+    ullam a dolorum natus ab. Tenetur.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos illum maiores,
+    iste officiis provident consequuntur labore, cumque distinctio, odio id
+    animi hic. Ex esse in laborum unde eaque! Nulla, libero nemo consectetur
+    excepturi alias ex quaerat necessitatibus nihil. Incidunt qui repudiandae
+    natus, totam quas voluptatum?
+  </p>
 </template>
 
 <script lang="ts" setup></script>
