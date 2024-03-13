6971405530abc4a26606ea0afb98e8dd6890d4f1
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-03-13 15:38:31 +0300

    feat(ProductPage.vue): Add appropriate column and row and add action column that contains delete and edit button

--- src/types/entityTypes.ts
+++ src/types/entityTypes.ts
@@ -2,3 +2,16 @@ export interface AuthInput {
   email: { value: string; valid: boolean };
   password: { value: string; valid: boolean };
 }
+export interface ProductRowType {
+  productId: number;
+  productName: string;
+  catagory: string;
+  desc: string;
+  productCondition: number;
+}
+export interface ProductColumnType {
+  name: string;
+  label: string;
+  field: (row: ProductRowType) => any;
+  align: "center" | "left" | "right";
+}
